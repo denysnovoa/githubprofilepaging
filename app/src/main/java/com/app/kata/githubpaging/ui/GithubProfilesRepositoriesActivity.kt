2b6ce258d443cda15cd0582f4537fa65c1367eae
Denys <dnovoa20@gmail.com>
2020-10-17 16:42:09 +0200

    added some resources

--- app/src/main/java/com/app/kata/githubpaging/ui/GithubProfilesRepositoriesActivity.kt
+++ app/src/main/java/com/app/kata/githubpaging/ui/GithubProfilesRepositoriesActivity.kt
@@ -1,5 +1,6 @@
 package com.app.kata.githubpaging.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.app.kata.githubpaging.ui.adapter.GithubProfileAdapter
@@ -14,6 +15,14 @@ class GithubProfilesRepositoriesActivity : AppCompatActivity() {
   private val adapter = GithubProfileAdapter()
   private var searchJob: Job? = null
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+
+  private fun updateRepoListFromInput() {
+
+  }
+
   private fun search(query: String) {
     searchJob?.cancel()
     searchJob = lifecycleScope.launch {
@@ -23,3 +32,4 @@ class GithubProfilesRepositoriesActivity : AppCompatActivity() {
     }
   }
 }
+
