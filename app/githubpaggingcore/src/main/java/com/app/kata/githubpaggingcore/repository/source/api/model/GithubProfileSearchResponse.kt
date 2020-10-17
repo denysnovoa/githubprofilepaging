cd623d5cd7e2efd03a71d2cd6dd8421c892206a6
Denys <dnovoa20@gmail.com>
2020-10-17 15:24:07 +0200

    added some Dto and Response models

---
+++ app/githubpaggingcore/src/main/java/com/app/kata/githubpaggingcore/repository/source/api/model/GithubProfileSearchResponse.kt
@@ -0,0 +1,9 @@
+package com.app.kata.githubpaggingcore.repository.source.api.model
+
+import com.google.gson.annotations.SerializedName
+
+data class GithubProfileSearchResponse(
+    @SerializedName("total_count") val total: Int = 0,
+    @SerializedName("items") val items: List<GithubProfileDto> = emptyList(),
+    val nextPage: Int? = null
+)
