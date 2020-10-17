12ed143f1a5746b5a2c9de72a256608366782091
Denys <dnovoa20@gmail.com>
2020-10-17 16:19:49 +0200

    added GithubProfilesRepositoriesActivity

--- app/src/main/java/com/app/kata/githubpagging/ui/adapter/GithubProfileAdapter.kt
+++ app/src/main/java/com/app/kata/githubpagging/ui/adapter/GithubProfileAdapter.kt
@@ -1,4 +1,10 @@
 package com.app.kata.githubpagging.ui.adapter
 
+import androidx.paging.PagingData
+import com.app.kata.githubpaggingcore.data.source.api.model.GithubProfileDto
+
 class GithubProfileAdapter {
+  fun submitData(it: PagingData<GithubProfileDto>) {
+    TODO("Not yet implemented")
+  }
 }
