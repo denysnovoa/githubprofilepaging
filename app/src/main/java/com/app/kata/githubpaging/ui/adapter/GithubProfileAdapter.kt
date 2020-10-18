452661a09eb5f95026aa938976cb24e0988eac94
Denys <dnovoa20@gmail.com>
2020-10-18 15:22:36 +0200

    render separator view holder

--- app/src/main/java/com/app/kata/githubpaging/ui/adapter/GithubProfileAdapter.kt
+++ app/src/main/java/com/app/kata/githubpaging/ui/adapter/GithubProfileAdapter.kt
@@ -2,41 +2,71 @@ package com.app.kata.githubpaging.ui.adapter
 
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.app.kata.githubpaging.R
+import com.app.kata.githubpaging.ui.model.GithubProfileUiModel
 import com.app.kata.githubpagingcore.data.source.api.model.GithubProfileDto
 
 class GithubProfileAdapter :
-  PagingDataAdapter<GithubProfileDto, RecyclerView.ViewHolder>(GITHUB_PROFILE_COMPARATOR) {
+  PagingDataAdapter<GithubProfileUiModel, RecyclerView.ViewHolder>(GITHUB_VIEW_MODEL_COMPARATOR) {
 
   override fun onCreateViewHolder(
     parent: ViewGroup,
     viewType: Int
   ): RecyclerView.ViewHolder {
-    return GithubProfileViewHolder.create(parent)
+    return if (viewType == R.layout.github_profile_view_item) {
+      GithubProfileViewHolder.create(parent)
+    } else {
+      GithubProfileSeparatorViewHolder.create(parent)
+    }
+  }
+
+  override fun getItemViewType(position: Int): Int {
+    return when (getItem(position)) {
+      is GithubProfileUiModel.Item -> R.layout.github_profile_view_item
+      is GithubProfileUiModel.SeparatorItem -> R.layout.github_profile_separator_view_item
+      null -> throw UnsupportedOperationException("Unknown view")
+    }
   }
 
   override fun onBindViewHolder(
     holder: RecyclerView.ViewHolder,
     position: Int
   ) {
-    getItem(position)?.let {
-      (holder as GithubProfileViewHolder).bind(it)
+    val uiModel = getItem(position)
+    uiModel.let {
+      when (uiModel) {
+        is GithubProfileUiModel.Item ->
+          (holder as GithubProfileViewHolder).bind(uiModel.githubProfile)
+        is GithubProfileUiModel.SeparatorItem -> (holder as GithubProfileSeparatorViewHolder).bind(
+          uiModel.description
+        )
+      }
     }
   }
 
   companion object {
 
-    private val GITHUB_PROFILE_COMPARATOR = object : DiffUtil.ItemCallback<GithubProfileDto>() {
-      override fun areItemsTheSame(oldItem: GithubProfileDto, newItem: GithubProfileDto): Boolean =
-        oldItem.fullName == newItem.fullName
+    private val GITHUB_VIEW_MODEL_COMPARATOR =
+      object : DiffUtil.ItemCallback<GithubProfileUiModel>() {
+        override fun areItemsTheSame(
+          oldItem: GithubProfileUiModel,
+          newItem: GithubProfileUiModel
+        ): Boolean {
+          return (oldItem is GithubProfileUiModel.Item
+              && newItem is GithubProfileUiModel.Item &&
+              oldItem.githubProfile.fullName == newItem.githubProfile.fullName) ||
+              (oldItem is GithubProfileUiModel.SeparatorItem &&
+                  newItem is GithubProfileUiModel.SeparatorItem &&
+                  oldItem.description == newItem.description)
+        }
 
-      override fun areContentsTheSame(
-        oldItem: GithubProfileDto,
-        newItem: GithubProfileDto
-      ): Boolean = oldItem == newItem
-
-    }
+        override fun areContentsTheSame(
+          oldItem: GithubProfileUiModel,
+          newItem: GithubProfileUiModel
+        ): Boolean =
+          oldItem == newItem
+      }
   }
 }
