b96db48f8abfa224d3f28828d3ec19ed691b5d78
Denys <dnovoa20@gmail.com>
2020-10-18 14:57:04 +0200

    added GithubProfileUiModel

---
+++ app/src/main/java/com/app/kata/githubpaging/ui/model/GithubProfileUiModel.kt
@@ -0,0 +1,13 @@
+package com.app.kata.githubpaging.ui.model
+
+import com.app.kata.githubpagingcore.data.source.api.model.GithubProfileDto
+
+sealed class GithubProfileUiModel {
+
+  data class Item(val githubProfile: GithubProfileDto) : GithubProfileUiModel() {
+    private val roundedStarCount: Int
+      get() = this.githubProfile.stars / 10_000
+  }
+
+  data class SeparatorItem(val description: String) : GithubProfileUiModel()
+}
