eedef3a9c1d6ef8281e7c6e9d087efe1bed4831c
Denys <dnovoa20@gmail.com>
2020-10-18 14:37:32 +0200

    added GithubProfileLoadState adapter and viewHolder

---
+++ app/src/main/java/com/app/kata/githubpaging/ui/adapter/GithubProfileLoadStateViewHolder.kt
@@ -0,0 +1,39 @@
+package com.app.kata.githubpaging.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.RecyclerView
+import com.app.kata.githubpaging.databinding.GithubProfilesLoadStateFooterViewItemBinding
+
+class GithubProfileLoadStateViewHolder(
+  private val binding: GithubProfilesLoadStateFooterViewItemBinding,
+  retry: () -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+  init {
+    binding.retryButton.setOnClickListener { retry.invoke() }
+  }
+
+  fun bind(loadState: LoadState) {
+    if (loadState is LoadState.Error) {
+      binding.errorMsg.text = loadState.error.localizedMessage
+    }
+    binding.progressBar.isVisible = loadState is LoadState.Loading
+    binding.retryButton.isVisible = loadState !is LoadState.Loading
+    binding.errorMsg.isVisible = loadState !is LoadState.Loading
+  }
+
+  companion object {
+
+    fun create(parent: ViewGroup, retry: () -> Unit) =
+      GithubProfileLoadStateViewHolder(
+        GithubProfilesLoadStateFooterViewItemBinding.inflate(
+          LayoutInflater.from(parent.context),
+          parent,
+          false
+        ), retry
+      )
+  }
+}
