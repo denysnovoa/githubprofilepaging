477432ca61f64da38c031dda336bc34b3ba98f5e
Denys <dnovoa20@gmail.com>
2020-10-18 15:34:29 +0200

    implemented database

--- app/githubpagingcore/src/main/java/com/app/kata/githubpagingcore/data/source/local/GithubProfileDao.kt
+++ app/githubpagingcore/src/main/java/com/app/kata/githubpagingcore/data/source/local/GithubProfileDao.kt
@@ -5,20 +5,20 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.app.kata.githubpagingcore.data.source.local.entity.GithubProfileEntity
+import com.app.kata.githubpagingcore.data.source.api.model.GithubProfileDto
 
 @Dao
 interface GithubProfileDao {
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
-  suspend fun insertAll(repos: List<GithubProfileEntity>)
+  suspend fun insertAll(repos: List<GithubProfileDto>)
 
   @Query(
     "SELECT * FROM github_profiles WHERE " +
         "name LIKE :queryString OR description LIKE :queryString " +
         "ORDER BY stars DESC, name ASC"
   )
-  fun reposByName(queryString: String): PagingSource<Int, GithubProfileEntity>
+  fun reposByName(queryString: String): PagingSource<Int, GithubProfileDto>
 
   @Query("DELETE FROM github_profiles")
   suspend fun clearRepos()
---
+++ app/githubpagingcore/src/main/java/com/app/kata/githubpagingcore/data/source/local/GithubProfileDatabase.kt
@@ -0,0 +1,31 @@
+package com.app.kata.githubpagingcore.data.source.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.app.kata.githubpagingcore.data.source.api.model.GithubProfileDto
+
+@Database(entities = [GithubProfileDto::class], version = 1, exportSchema = false)
+abstract class GithubProfileDatabase : RoomDatabase() {
+
+  abstract fun githubProfileDao(): GithubProfileDao
+
+  companion object {
+
+    @Volatile
+    private var INSTANCE: GithubProfileDatabase? = null
+
+    fun getInstance(context: Context): GithubProfileDatabase =
+      INSTANCE ?: synchronized(this) {
+        INSTANCE
+          ?: buildDatabase(context).also { INSTANCE = it }
+      }
+
+    private fun buildDatabase(context: Context) =
+      Room.databaseBuilder(
+        context.applicationContext,
+        GithubProfileDatabase::class.java, "Github.db"
+      ).build()
+  }
+}
